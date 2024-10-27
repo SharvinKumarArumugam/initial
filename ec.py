c5bb9063e989fe4db07cdea328f8cff3de99f6bf
Mr.Sharvin <s21a0053@siswa.umk.edu.my>
2024-10-27 11:32:14 +0800

    Update ec.py

--- ec.py
+++ ec.py
@@ -12,7 +12,7 @@ POP_SIZE = 500
 
 #MUT_RATE: Rate at which our string will be changed.
 #MUT_RATE = 0.2
-MUT_RATE = st.number_input("Enter your mutation rate","sharvin")
+MUT_RATE = st.number_input("Enter your mutation rate")
 #TARGET: Our goal.
 #TARGET = 'Sharvin'
 TARGET = st.text_input("Enter your name","sharvin")
